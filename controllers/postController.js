026f9ffe89592958236c3622deb24f56e7f99e45
dhamolahedonist <dhamolahedonist>
2023-10-26 19:51:15 +0100

    created an endpoint to delete a post by the authorized user

--- controllers/postController.js
+++ controllers/postController.js
@@ -37,7 +37,44 @@ const getSinglePost = async (req, res) => {
   }
 };
 
+const getAllPost = async (req, res) => {
+  console.log(req);
+  try {
+    const posts = await Post.find();
+    console.log(posts);
+
+    res.status(200).json({ posts });
+  } catch (error) {
+    res.status(500).json({ error: "Error fetching posts" });
+  }
+};
+
+const deletePost = async (req, res) => {
+  try {
+    const { postId } = req.params;
+    const post = await Post.findById(postId);
+
+    if (!post) {
+      return res.status(404).json({ error: "Post not found" });
+    }
+    const userIdFromToken = req.user.userId;
+    if (post.user.toString() !== userIdFromToken) {
+      return res
+        .status(403)
+        .json({ error: "You are not authorized to delete this post" });
+    }
+
+    const deletedPost = await Post.findByIdAndRemove(postId);
+
+    res.status(200).json({ message: "Post deleted successfully", deletedPost });
+  } catch (error) {
+    res.status(500).json({ error: "Error deleting post" });
+  }
+};
+
 module.exports = {
   createPost,
   getSinglePost,
+  getAllPost,
+  deletePost,
 };
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,10 +1,17 @@
 const express = require("express");
-const { createPost, getSinglePost } = require("../controllers/postController");
+const {
+  createPost,
+  getSinglePost,
+  getAllPost,
+  deletePost,
+} = require("../controllers/postController");
 const { verifyToken } = require("../middleware/validation/validateUser");
 
 const postRoute = express.Router();
 
 postRoute.post("/create", verifyToken, createPost);
 postRoute.get("/:postId", getSinglePost);
+postRoute.get("/allposts", getAllPost);
+postRoute.delete("/posts/:postId", verifyToken, deletePost);
 
 module.exports = postRoute;
